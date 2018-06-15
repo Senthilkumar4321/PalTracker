dbe2a94fdee91add26596a4122faff5bd448e5d9
Mike Gehard <mgehard@pivotal.io>
2018-06-15 10:40:04 -0600

    PAL tracker .net core web api

--- src/PalTracker/WelcomeController.cs
+++ src/PalTracker/WelcomeController.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
+using PalTracker;
 
 namespace PalTracker
 {
     [Route("/")]
     public class WelcomeController : ControllerBase
     {
+        public WelcomeMessage _message{get;set;}
+
+        public WelcomeController(WelcomeMessage Message){
+            _message=Message;
+        }
+
         [HttpGet]
         public string SayHello()=>"hello";
+
+        [HttpGet]
+        public string RandController(){
+            return _message.Message;
+        }
     }
   [Route("/End")]
       public class EndController : ControllerBase
